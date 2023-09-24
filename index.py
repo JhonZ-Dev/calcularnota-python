b3cda510b656bc1ffd236d067debab300493465e
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-23 22:36:40 -0500

    Update index.py

--- index.py
+++ index.py
@@ -2,3 +2,4 @@ nota_parcial1 = float(input("Ingresa la nota del primer parcial: "))
 nota_parcial2 = float(input("Ingresa la nota del segundo parcial: "))
 nota_parcial3 = float(input("Ingresa la nota del tercer parcial: "))
 # Calcular el total de las notas
+total_notas = nota_parcial1 + nota_parcial2 + nota_parcial3
