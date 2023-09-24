30146acbefc91ef7215ff29e023cc6368a99bfa5
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-23 22:41:52 -0500

    Update notasvalidadas.py

--- notasvalidadas.py
+++ notasvalidadas.py
@@ -33,4 +33,9 @@ while nota_parcial3 is None:
 # Calcular el total de las notas
 total_notas = nota_parcial1 + nota_parcial2 + nota_parcial3
 
+# Verificar si el estudiante aprobó
+if total_notas > 14:
+    print("El estudiante está aprobado con un total de", total_notas, "puntos.")
+else:
+    print("El estudiante está reprobado con un total de", total_notas, "puntos.")
 
