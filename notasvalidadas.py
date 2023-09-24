ffe0f470622eba71015b3c1347cd42b3a3e873e7
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-09-23 22:40:22 -0500

    Update notasvalidadas.py

--- notasvalidadas.py
+++ notasvalidadas.py
@@ -6,4 +6,5 @@ nota_parcial3 = None
 # Solicitar las notas hasta que no sean nulas
 while nota_parcial1 is None:
     nota_parcial1_str = input("Ingresa la nota del primer parcial: ")
+    if nota_parcial1_str.strip():  # Verificar que no sea una cadena vacía
 
